2c7268f17ee99df71ce80dd89b3125967b398493
Hossam-Handanovic <69869605+Hossam-Handanovic@users.noreply.github.com>
2021-07-07 08:15:11 +0200

    DSS

---
+++ DSS Section22/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace DSS_Section22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEV = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEV
+            // 
+            this.btnEV.Location = new System.Drawing.Point(182, 248);
+            this.btnEV.Name = "btnEV";
+            this.btnEV.Size = new System.Drawing.Size(143, 23);
+            this.btnEV.TabIndex = 0;
+            this.btnEV.Text = "Get The best Alternative";
+            this.btnEV.UseVisualStyleBackColor = true;
+            this.btnEV.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(202, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(343, 101);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(74, 157);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(202, 157);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(343, 157);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(74, 209);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(202, 209);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 8;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(343, 209);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 9;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(106, 75);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(31, 20);
+            this.textBox11.TabIndex = 10;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(237, 75);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(31, 20);
+            this.textBox12.TabIndex = 11;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(376, 75);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(31, 20);
+            this.textBox13.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Get EVPI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(113, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "S1";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(383, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "S3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(243, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "S2";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(23, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "d1";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(23, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "d2";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(23, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "d3";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(524, 312);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnEV);
+            this.Name = "Form1";
+            this.Text = "Decision Analysis under Risk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnEV;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ DSS Section22/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSS_Section22
+{
+    public partial class Form1 : Form
+    {
+        private double best;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double d1_s1 = double.Parse(textBox2.Text);
+            double d1_s2 = double.Parse(textBox3.Text);
+            double d1_s3 = double.Parse(textBox4.Text);
+            double d2_s1 = double.Parse(textBox5.Text);
+            double d2_s2 = double.Parse(textBox6.Text);
+            double d2_s3 = double.Parse(textBox7.Text);
+            double d3_s1 = double.Parse(textBox8.Text);
+            double d3_s2 = double.Parse(textBox9.Text);
+            double d3_s3 = double.Parse(textBox10.Text);
+            double s1 = double.Parse(textBox11.Text);
+            double s2 = double.Parse(textBox12.Text);
+            double s3 = double.Parse(textBox13.Text);
+
+            double EV_under_Risk1 = (d1_s1 * s1) + (d1_s2 * s2) + (d1_s3 * s3);
+            double EV_under_Risk2 = (d2_s1 * s1) + (d2_s2 * s2) + (d2_s3 * s3);
+            double EV_under_Risk3 = (d3_s1 * s1) + (d3_s2 * s2) + (d3_s3 * s3);
+           
+            if (EV_under_Risk1 > EV_under_Risk2 && EV_under_Risk1 > EV_under_Risk3)
+            {
+                MessageBox.Show("The best alternative is " + Convert.ToString(EV_under_Risk1));
+
+                  best = EV_under_Risk1;
+
+            }
+            else if (EV_under_Risk2 > EV_under_Risk1 && EV_under_Risk2 > EV_under_Risk3)
+            {
+                MessageBox.Show("The best alternative is " + Convert.ToString(EV_under_Risk2));
+                 best = EV_under_Risk2;
+            }
+            else
+            {
+                MessageBox.Show("The best alternative is " + Convert.ToString(EV_under_Risk3));
+                  best = EV_under_Risk3;
+            }
+        }
+
+       
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double d1_s1 = double.Parse(textBox2.Text);
+            double d1_s2 = double.Parse(textBox3.Text);
+            double d1_s3 = double.Parse(textBox4.Text);
+            double d2_s1 = double.Parse(textBox5.Text);
+            double d2_s2 = double.Parse(textBox6.Text);
+            double d2_s3 = double.Parse(textBox7.Text);
+            double d3_s1 = double.Parse(textBox8.Text);
+            double d3_s2 = double.Parse(textBox9.Text);
+            double d3_s3 = double.Parse(textBox10.Text);
+            double s1 = double.Parse(textBox11.Text);
+            double s2 = double.Parse(textBox12.Text);
+            double s3 = double.Parse(textBox13.Text);
+            double EV1;
+            double EV2;
+            double EV3;
+            if (d1_s1>d2_s1 && d1_s1 > d3_s1)
+            {
+                EV1 = d1_s1 * s1;
+            }
+            else if (d2_s1 > d1_s1 && d2_s2 > d3_s1)
+            {
+                EV1 = d2_s1 * s1;
+            }
+            else
+            {
+                EV1 = d3_s1 * s1;
+            }
+
+            if (d1_s2 > d2_s2 && d1_s2 > d3_s2)
+            {
+                EV2 = d1_s2 * s2;
+            }
+            else if (d2_s2 > d1_s2 && d2_s2 > d3_s2)
+            {
+                EV2 = d2_s2 * s2;
+            }
+            else
+            {
+                EV2 = d3_s2 * s2;
+            }
+
+            if (d1_s3 > d2_s3 && d1_s3 > d3_s3)
+            {
+                EV3 = d1_s3 * s3;
+            }
+            else if (d2_s3 > d1_s3 && d2_s3 > d3_s3)
+            {
+                EV3 = d2_s3 * s3;
+            }
+            else
+            {
+                EV3 = d3_s3 * s3;
+            }
+
+            double EV_Under_Certainty = EV1 + EV2 + EV3;
+
+            double EVPI = EV_Under_Certainty - best;
+
+            MessageBox.Show("The expected value of perefect information " + EVPI);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
